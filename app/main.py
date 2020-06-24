6967cbdad73dd977107e12b151ef9891c4592352
shunk-py <auau3700@gmail.com>
2020-06-24 23:09:12 +0900

    fix docker files

--- app/main.py
+++ app/main.py
@@ -1,11 +1,11 @@
 from fastapi import FastAPI
-from service import TTTService
 
 
 app = FastAPI(
     title="Japan Algo Stock Dev",
     version="1.0.0"
 )
+
 
 @app.get("/")
 async def get_internal_wallets():
